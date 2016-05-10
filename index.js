364db7a9bdbceaa6c8dfe6e44aa789af5401937a
Matt Perkins <mperkins@redhat.com>
2016-05-10 16:39:59 -0400

    v1

---
+++ index.js
@@ -0,0 +1,54 @@
+const LRS = require('./src/tincanfacade.js');
+
+// Set initial statement data
+let testStatement = {
+        subjectName: 'Blue Berry',
+        subjectID: 'blueberry@pietown.com',
+        verbDisplay: 'completed',
+        objectName: 'Filling the pies',
+        objectType: 'course',
+        objectID: 'http://pietown.com/Apple_Pie_Filling_101',
+        statement: 'Click Submit to create a statement',
+        isError: false
+    },
+    defaults = {
+        result: {
+            completion: true,
+            success: true,
+            score: {
+                scaled: 1
+            }
+        },
+        authority: {
+            name: 'Irene Instructor',
+            mbox: 'mailto:irene@example.com'
+        },
+        context: {
+            instructor: {
+                name: 'Irene Instructor',
+                mbox: 'mailto:irene@example.com'
+            },
+            contextActivities: {
+                parent: {
+                    id: 'http://example.com/activities/pie-filling-101'
+                },
+                grouping: {
+                    id: 'http://example.com/activities/pie-town-school'
+                }
+            }
+        }
+    };
+
+// Connect to the LRS
+LRS.connect({
+    end: 'https://lrs.adlnet.gov/xapi/',
+    user: '',
+    pass: ''
+}).then(() => {
+    console.log('Connected to the LRS');
+    // Set defaults for every statement
+    LRS.default(defaults);
+    // Send test statement
+    LRS.send(testStatement).then(() => console.log('Statement sent'))
+        .catch((err) => console.warn('Failed to send', err));
+}).catch((err) => console.warn('Failed to connect to LRS', err));
--- src/tincanfacade.js
+++ src/tincanfacade.js
@@ -7,122 +7,136 @@
  * Statment generator https://tincanapi.com/statement-generator/
  */
 
-import TinCan from 'tincanjs';
-import VerbDictionary from './ADL-Verbs';
-import ActivityDictionary from './ADL-Activity';
-import {defaults, assign} from 'lodash';
+const TinCan = require('tincanjs');
+const VerbDictionary = require('./ADL-Verbs');
+const ActivityDictionary = require('./ADL-Activity');
+const _ = require('lodash');
 
 let LRS,
-    defaultProps    = {},
-    verbURLPrefix   = 'http://adlnet.gov/expapi/verbs/',
+    defaultProps = {},
+    verbURLPrefix = 'http://adlnet.gov/expapi/verbs/',
     activityURLPrefix = 'http://adlnet.gov/expapi/activities/',
     defaultLanguage = 'en-US';
 
-export default {
+module.exports = {
 
-  // Connect to the LRS
-  connect(initObject) {
-    let {end, user, pass} = initObject;
-    return new Promise(function (resolve, reject) {
-      try {
-        LRS = new TinCan.LRS({
-          endpoint : end,
-          username : user,
-          password : pass,
-          allowFail: false
+    // Connect to the LRS
+    connect(initObject) {
+        let {
+            end, user, pass
+        } = initObject;
+        return new Promise(function(resolve, reject) {
+            try {
+                LRS = new TinCan.LRS({
+                    endpoint: end,
+                    username: user,
+                    password: pass,
+                    allowFail: false
+                });
+                resolve();
+            } catch (err) {
+                reject(err);
+            }
         });
-        resolve();
-      } catch (err) {
-        reject(err);
-      }
-    });
-  },
+    },
 
-  // Set defaults to be applied to each statement such as actor or authority
-  default(defaultObj) {
-    defaultProps = assign({}, defaultObj);
-  },
+    // Set defaults to be applied to each statement such as actor or authority
+    default (defaultObj) {
+        defaultProps = _.assign({}, defaultObj);
+    },
 
-  // Returns array of verbs from the ADL list
-  getVerbsList() {
-    // return Object.keys(VerbDictionary).map((key) => VerbDictionary[key].display[defaultLanguage]);
-    return this.getDictionaryWordsList(VerbDictionary);
-  },
+    // Returns array of verbs from the ADL list
+    getVerbsList() {
+        // return Object.keys(VerbDictionary).map((key) => VerbDictionary[key].display[defaultLanguage]);
+        return this.getDictionaryWordsList(VerbDictionary);
+    },
 
-  // True if the verb is on the ADL list
-  validateVerb(verb) {
-    return this.getVerbsList().indexOf(verb.toLowerCase()) >= 0;
-  },
+    // True if the verb is on the ADL list
+    validateVerb(verb) {
+        return this.getVerbsList().indexOf(verb.toLowerCase()) >= 0;
+    },
 
-  // Returns array of activity from the ADL list
-  getActivitiesList() {
-    return this.getDictionaryWordsList(ActivityDictionary);
-  },
+    // Returns array of activity from the ADL list
+    getActivitiesList() {
+        return this.getDictionaryWordsList(ActivityDictionary);
+    },
 
-  // True if the activity is on the ADL list
-  validateActivity(activity) {
-    return this.getActivitiesList().indexOf(activity.toLowerCase()) >= 0;
-  },
+    // True if the activity is on the ADL list
+    validateActivity(activity) {
+        return this.getActivitiesList().indexOf(activity.toLowerCase()) >= 0;
+    },
 
-  getDictionaryWordsList(dictionary) {
-    return Object.keys(dictionary).map((key) => dictionary[key].display[defaultLanguage]);
-  },
+    getDictionaryWordsList(dictionary) {
+        return Object.keys(dictionary).map((key) => dictionary[key].display[defaultLanguage]);
+    },
 
-  // Create an xAPI statement object
-  createStatement(statementData) {
-    let statement,
-        {subjectName, subjectID, verbDisplay, objectID, objectType, objectName} = statementData;
+    // Create an xAPI statement object
+    createStatement(statementData) {
+        let statement, {
+            subjectName, subjectID, verbDisplay, objectID, objectType, objectName
+        } = statementData;
 
-    if (!this.validateVerb(verbDisplay)) {
-      console.warn('LRS verb is not in the dictionary', verbDisplay);
+        if (!this.validateVerb(verbDisplay)) {
+            console.warn('LRS verb is not in the dictionary', verbDisplay);
+        }
+
+        statement = _.defaults({
+            actor: {
+                name: subjectName,
+                mbox: 'mailto:' + subjectID
+            },
+            verb: {
+                id: verbURLPrefix + verbDisplay.toLowerCase(),
+                display: {
+                    'en-US': verbDisplay.toLowerCase()
+                }
+            },
+            object: {
+                id: objectID,
+                definition: {
+                    type: objectType ? activityURLPrefix + objectType : null,
+                    name: {
+                        'en-US': objectName
+                    }
+                }
+            }
+        }, defaultProps);
+        return new TinCan.Statement(statement);
+    },
+
+    // Create and send an xAPI statement
+    send(statementData) {
+        let statement = this.createStatement(statementData);
+        console.log(statement);
+
+        return new Promise(function(resolve, reject) {
+
+            if (!LRS) {
+                reject({
+                    message: 'LRS not connected! Cannot send statement: ' + statement
+                });
+                return;
+            }
+
+            LRS.saveStatement(statement, {
+                callback: (err, xhr) => {
+                    if (err !== null) {
+                        if (xhr !== null) {
+                            reject({
+                                status: xhr.status,
+                                message: xhr.responseText
+                            });
+                            return;
+                        }
+                        reject({
+                            message: err
+                        });
+                        return;
+                    }
+                    resolve();
+                }
+            });
+        });
     }
 
-    statement = defaults({
-      actor : {
-        name: subjectName,
-        mbox: 'mailto:' + subjectID
-      },
-      verb  : {
-        id     : verbURLPrefix + verbDisplay.toLowerCase(),
-        display: {'en-US': verbDisplay.toLowerCase()}
-      },
-      object: {
-        id        : objectID,
-        definition: {
-          type      : objectType ? activityURLPrefix + objectType : null,
-          name: {'en-US': objectName}
-        }
-      }
-    }, defaultProps);
-    return new TinCan.Statement(statement);
-  },
-
-  // Create and send an xAPI statement
-  send(statementData) {
-    let statement = this.createStatement(statementData);
-    console.log(statement);
-
-    return new Promise(function (resolve, reject) {
-
-      if (!LRS) {
-        reject({message: 'LRS not connected! Cannot send statement: ' + statement});
-        return;
-      }
-
-      LRS.saveStatement(statement, {
-        callback: (err, xhr) => {
-          if (err !== null) {
-            if (xhr !== null) {
-              reject({status: xhr.status, message: xhr.responseText});
-              return;
-            }
-            reject({message: err});
-            return;
-          }
-          resolve();
-        }
-      });
-    });
-  }
-
-}
+};
